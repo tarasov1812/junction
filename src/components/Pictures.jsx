329c129f92025af11f4d48d4840314d17a359dda
tarasov1812 <tarasov1812@gmail.com>
2023-10-22 13:40:39 +0200

    Update Pictures.jsx

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import styles from '../styles/Pictures.module.css';
 
+
+const el = document.getElementById("root");
+el.getAttribute('data-param');
+
+
 function Pictures() {
   return (
     <>
@@ -16,7 +21,7 @@ function Pictures() {
       </a>
       <a className={styles.repost}>
         <img src="/img/close.png" alt="Share Icon" />
-        <span>Close</span>
+        <span>{el.getAttribute('data-param')}</span>
       </a>
     </div>
     <img className={styles.imeg} src="/img/picture.png"></img>
